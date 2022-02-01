f5ce44ae953292a40368962b8f39f36a31890f1c
GeorgeLR87 <jorgelrgarduno@gmail.com>
2022-02-01 16:41:40 -0600

    Alerta presupuesto invalido

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import { useState } from 'react';
+import Mensaje from './Mensaje';
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+
+    const [mensaje, setMensaje] = useState('')
+ 
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+
+        if(!Number(presupuesto) || Number(presupuesto) < 0 ) {
+            setMensaje('No es un presupuesto válido');
+        } else {
+            console.log('Si es un presupuesto válido');
+        }
+    }
+
   return (
   <div className='contenedor-presupuesto contenedor sombra'>
       
-      <form className='formulario'>
+      <form onSubmit={handlePresupuesto} className='formulario'>
           <div className='campo'>
               <label>Definir Presupuesto</label>
 
               <input 
                 className='nuevo-presupuesto'
                 type='text'
-                placeholder='Añade tu Presupuesto'                             
+                placeholder='Añade tu Presupuesto'   
+                value={presupuesto}             
+                onChange={ e => setPresupuesto(e.target.value)}             
               />
           </div>
 
           <input type='submit' value='Añadir' />
+
+          {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje> }
       </form>
   </div>
   );
