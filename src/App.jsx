526788dd8a1670d94e98e557226c48d6ff7554de
GeorgeLR87 <jorgelrgarduno@gmail.com>
2022-02-02 13:27:56 -0600

    Componente Crear Gasto, Mostrar Gastos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import ListadoGastos from './components/ListadoGastos'
 import Modal  from './components/Modal'
 import { generarId } from './helpers'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
@@ -26,6 +27,7 @@ function App() {
 
   const guardarGasto = gasto => {
     gasto.id = generarId()
+    gasto.fecha = Date.now()
     setGastos([...gastos, gasto])
 
     setAnimarModal(false)
@@ -44,14 +46,24 @@ function App() {
     />
 
     {isValidPresupuesto && (
+      <>
+
+      <main>
+        <ListadoGastos
+          gastos={gastos}
+        />
+        
+      </main>
 
        <div className='nuevo-gasto'>
-         <img 
-           src={IconoNuevoGasto}
-          alt='icono nuevo gasto'    
-          onClick={handleNuevoGasto}  
-         />
+          <img 
+            src={IconoNuevoGasto}
+            alt='icono nuevo gasto'    
+            onClick={handleNuevoGasto}  
+          />
       </div>
+
+      </>
     )}
 
     {modal && <Modal 
