9ecc16f77beb4c025f4758e2eece3cfc4ee9a72a
GeorgeLR87 <jorgelrgarduno@gmail.com>
2022-02-01 20:36:50 -0600

    Formulario de Gastos, Creando State para los Gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react'
 import CerrarBtn from '../img/cerrar.svg'
 
 
-const Modal = ({setModal}) => {
+const Modal = ({setModal, animarModal, setAnimarModal}) => {
+
+    const [nombre, setNombre] = useState('')
+    const [cantidad, setCantidad] = useState('')
+    const [categoria, setCategoria] = useState('')
     
     const ocultarModal = () => {
-        setModal(false)
+        
+        setAnimarModal(false)
+
+        setTimeout(() => {
+            setModal(false)
+        }, 500)
     }
     
   return (
@@ -15,9 +25,59 @@ const Modal = ({setModal}) => {
             alt='cerrar modal'
             onClick={ocultarModal}
           />
-
       </div>
-  </div>);
-};
+
+      <form className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
+          <legend>Nuevo Gasto</legend>
+
+          <div className='campo'>
+              <label htmlFor='nombre'>Nombre Gastos</label>
+              <input 
+                id='nombre'
+                type='text'
+                placeholder='Añade el Nombre del Gasto'
+                value={nombre}
+                onChange={ e => setNombre(e.target.value)}
+              />
+          </div>
+
+          <div className='campo'>
+              <label htmlFor='cantidad'>Cantidad</label>
+              <input 
+                id='cantidad'
+                type='number'
+                placeholder='Añade La cantidad del gasto: ej. 300'
+                value={cantidad}
+                onChange={ e => setCantidad(Number(e.target.value))}
+              />
+          </div>
+
+          <div className='campo'>
+              <label htmlFor='categoria'>Categoría</label>
+              
+              <select 
+                id="categoria"  
+                value={categoria}
+                onChange={ e => setCategoria(e.target.value)}              
+              >
+                <option value="">-- Seleccione --</option>
+                <option value="ahorro">Ahorro</option>
+                <option value="comida">Comida</option>
+                <option value="casa">Casa</option>
+                <option value="gastos">Gastos Varios</option>
+                <option value="ocio">Ocio</option>
+                <option value="salud">Salud</option>
+                <option value="suscripciones">Suscripciones</option>
+              </select>
+          </div>
+
+          <input 
+            type='submit'
+            value='Añadir Gasto'
+          />
+      </form>
+  </div>
+  )
+}
 
 export default Modal;
